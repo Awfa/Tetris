5c4a3394c5dcdc1d49c359ab66bcc11767358a91
Anthony Ha <anthonyha96@gmail.com>
2013-12-23 16:53:42 -0800

    Drew and made pieces move

--- tetris/src/com/anthonyha/tetris/Tetris.java
+++ tetris/src/com/anthonyha/tetris/Tetris.java
@@ -4,41 +4,36 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Tetris implements ApplicationListener {
 	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+	private ShapeRenderer shapeRenderer;
+	
+	private TetrisBoard gameBoard;
 	
 	@Override
 	public void create() {		
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
+		int scale = 30;
+		gameBoard = new TetrisBoard(scale);
 		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w, h);
 		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.translate(w/2-scale*(gameBoard.gameGrid.getWidth()/2), h/2-scale*(gameBoard.gameGrid.getHeight()/2), 0);
+		shapeRenderer.scale(scale, scale, 1);
 		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		
+
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+		shapeRenderer.dispose();
 	}
 
 	@Override
@@ -46,10 +41,35 @@ public class Tetris implements ApplicationListener {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		gameBoard.update(Gdx.graphics.getDeltaTime());
+		
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		
+		//Render GameBoard
+		shapeRenderer.setColor(0, 1, 0, 1);
+		
+		for (int x = 0; x < gameBoard.gameGrid.getWidth(); ++x) {
+			for (int y = 0; y < gameBoard.gameGrid.getHeight(); ++y) {
+				if (gameBoard.gameGrid.getValue(x, y)) {
+					shapeRenderer.rect(x, y, 1, 1);
+				}
+			}
+		}
+		
+		//Render Active Tetromino
+		shapeRenderer.setColor(1, 0, 0, 1);
+		
+		for (int x = 0; x < gameBoard.activeTetromino.blockGrid.getWidth(); ++x) {
+			for (int y = 0; y < gameBoard.activeTetromino.blockGrid.getHeight(); ++y) {
+				if (gameBoard.activeTetromino.blockGrid.getValue(x, y)) {
+					shapeRenderer.rect(x+gameBoard.tetrominoX, y+gameBoard.tetrominoY, 1, 1);
+				}
+			}
+		}
+		
+		shapeRenderer.end();
 	}
 
 	@Override
